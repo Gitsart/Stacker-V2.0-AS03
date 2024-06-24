90262744c771ece3d7e66ad73a5828bf0dc9d795
Gitsart <142268471+Gitsart@users.noreply.github.com>
2024-06-24 05:40:26 +0530

    Add files via upload

---
+++ Site_Iteration_22June/Site_Iteration_22June.ino
@@ -0,0 +1,160 @@
+#include <MobaTools.h>
+
+const byte stepPin = 18;
+const byte dirPin = 19;
+const int stepsPerRev = 400;
+const long collectPos = 2800;
+
+#define startPin 6  // Pin connected to the push button
+#define limitPin 9  // Reference switch
+const byte senPin = 8; // Cloth sensor
+#define solen 25  // Relay switch pin
+int buttonPressed = 0; // To track if the button is pressed
+bool motorShouldRun = false;  // To track if the motor should be running
+bool clothDetected = false;
+
+int fwdSpeed = 400;
+int bwdSpeed = 1000;
+
+int limit_count = 0;
+bool limswitch = 0;
+
+int hOme = 0;
+int pos = 0;
+
+unsigned long forwardStartTime = 0;  // Track the start time of forward motion
+bool relayActivated = false;  // Track the relay activation state
+
+MoToStepper stepper1(stepsPerRev, STEPDIR);
+
+void setup() {
+  Serial.begin(115200);
+  stepper1.attach(stepPin, dirPin);
+  stepper1.setRampLen(400);
+
+  pinMode(startPin, INPUT_PULLUP);  // Use internal pull-up resistor
+  pinMode(limitPin, INPUT_PULLUP);
+  pinMode(senPin, INPUT_PULLUP);
+  pinMode(solen, OUTPUT);
+
+  homing();
+  set_pos();
+}
+
+void loop() {
+  Serial.print("buttonPressed:" + buttonPressed);
+  if (digitalRead(startPin) == LOW) {
+    buttonPressed = 1;
+  }
+
+  if ((hOme == 2) && (limit_count >= 1) && (pos == 1) && (buttonPressed == 1)) {
+    int sen = digitalRead(senPin);
+    if (sen == 0) {
+      clothDetected = true;
+      motor();
+    }
+    Serial.print("CLOTH DETECTED:"); Serial.println(clothDetected);
+  }
+
+
+}
+
+void homing() {
+  stepper1.setRampLen(400);
+  stepper1.setSpeed(600);
+  while (digitalRead(limitPin) != HIGH && limit_count == 0) {
+    stepper1.rotate(-1);
+    Serial.print(limswitch);
+    Serial.println("Finding END");
+  }
+
+  if (digitalRead(limitPin) == HIGH && limit_count == 0) {
+    stepper1.stop();
+    Serial.println("END POSITION");
+    limit_count += 1;
+    limswitch = HIGH;
+    hOme = 1;
+  }
+  stepper1.setZero();
+  delay(2000);
+  stepper1.setSpeed(500);
+  stepper1.moveTo(400);
+  limswitch = LOW;
+  delay(2000);
+  while (digitalRead(limitPin) != HIGH && limit_count == 1) {
+    stepper1.rotate(-1);
+    Serial.print(limswitch);
+    Serial.println("Finding END2");
+  }
+  if (digitalRead(limitPin) == HIGH && limit_count == 1) {
+    stepper1.stop();
+    Serial.println("END POSITION2");
+    limit_count += 1;
+    limswitch = HIGH;
+    hOme = 2;
+  }
+  Serial.print("limit_count:"); Serial.println(limit_count);
+  Serial.print("limswitch:"); Serial.println(limswitch);
+  Serial.print("hOme:"); Serial.println(hOme);
+  Serial.print("POS:"); Serial.println(pos);
+  stepper1.setZero();
+  delay(2000);
+}
+
+void set_pos() {
+  while (hOme == 2 && limit_count > 1 && limswitch == HIGH && pos == 0) {
+    delay(1000);
+    stepper1.setRampLen(200);
+    stepper1.setSpeed(600);
+    stepper1.moveTo(collectPos);
+
+    while (stepper1.moving()) {
+      Serial.println("MOVING to Pos1");
+    }
+
+    pos = 1;
+    Serial.println("StartPositionSet, READY TO USE");
+    digitalWrite(solen, HIGH);
+    buttonPressed = 0;
+  }
+  Serial.print("POS:"); Serial.println(pos);
+}
+
+void motor() {
+  if (clothDetected) {
+    stepper1.setRampLen(100);
+    // Move forward while sensor is detecting cloth (value is 0)
+    stepper1.setSpeed(fwdSpeed);
+    forwardStartTime = millis();  // Record the start time of forward motion
+    relayActivated = false;  // Ensure relay is initially deactivated
+
+    while ((digitalRead(senPin) == 0) && (digitalRead(limitPin) != HIGH)) {
+      stepper1.rotate(-1);
+      delay(500);
+
+      if (millis() - forwardStartTime >= 5500 && !relayActivated) {
+        digitalWrite(solen, LOW);
+        relayActivated = true;
+      }
+      if (digitalRead(limitPin) == HIGH) {
+        stepper1.stop();
+      }
+
+      delay(1);  // Small delay to prevent motor stuttering
+    }
+    stepper1.stop();
+    digitalWrite(solen, HIGH);
+    // Move backward to the predefined collecting position
+    stepper1.setSpeed(bwdSpeed);
+    stepper1.moveTo(collectPos);
+    Serial.println("Moving backward to collecting position");
+
+    while (stepper1.moving()) {
+      // No delay here to ensure continuous movement
+    }
+
+    motorShouldRun = false;  // Reset the flag when the complete oscillation is done
+    clothDetected = false;  // Reset cloth detection flag
+    relayActivated = false;  // Reset relay activation flag
+  }
+}
---
+++ V1_codeIV2_WithSolenoidRelay/V1_codeIV2_WithSolenoidRelay.ino
@@ -0,0 +1,209 @@
+#include<MobaTools.h>
+const byte stepPin = 18;
+const byte dirPin = 19;
+
+#define limitPin 9
+const byte senPin = 8;
+#define startPin 6
+#define collectPin 5
+#define solen 25
+
+const int stepsperRev = 400;
+
+
+long  nextPos;
+const long targetPos = 2200;
+
+int count = 0;
+int limit_count = 0;
+
+bool limswitch = 0;
+
+int hOme = 0;
+int pos = 0;
+int sTart = 0;
+int cloth = 0;
+bool relayActivated = false;
+
+unsigned long forwardStartTime =0;
+
+
+int cloth_count = 0;
+
+MoToStepper stepper1(stepsperRev, STEPDIR);
+MoToTimer stepperPause;
+
+bool stepperRunning;
+bool movingForward;
+bool clothDetected;
+
+
+void setup() {
+  Serial.begin(9600);
+  stepper1.attach(stepPin, dirPin);
+  stepper1.setRampLen(20);
+
+  stepperRunning = false;
+  movingForward = true;
+  clothDetected = false;
+
+
+  pinMode(limitPin, INPUT_PULLUP);
+
+
+  pinMode(senPin, INPUT_PULLUP);
+
+  pinMode(solen, OUTPUT);
+
+  pinMode(collectPin, INPUT_PULLUP);
+
+
+  homing();
+  set_pos();
+
+}
+
+void loop() {
+  int c = digitalRead(collectPin);
+
+
+  while (digitalRead(startPin) == LOW)
+  {
+    sTart = 1;
+  }
+
+
+  if ((hOme == 2) && (limit_count > 1) && (count == 0) && (pos = 1) && (sTart == 1))
+  {
+    int sen = digitalRead(senPin);
+    bool sensorFound = false;
+
+    if (sen == 0 || sen == 1)
+    {
+      sensorFound = true;
+    }
+
+    if (sen == 0) {
+      clothDetected = false;
+      motor();
+      Serial.print("CLOTHCOUNT:"); Serial.println(cloth_count);
+
+    }
+  }
+  if (c == 0) {
+    collect();
+    homing();
+    set_pos();
+  }
+}
+
+void homing() {
+  stepper1.setSpeed(800);
+  while (digitalRead(limitPin) != HIGH && limit_count == 0) {
+    stepper1.rotate(-1);
+    Serial.print(limswitch);
+    Serial.println("Finding END");
+  }
+
+  if (digitalRead(limitPin) == HIGH && limit_count == 0) {
+    stepper1.stop();
+    Serial.println("END POSITION");
+    limit_count += 1;
+    limswitch = HIGH;
+    hOme = 1;
+  }
+  stepper1.setZero();
+  delay(2000);
+  stepper1.setSpeed(500); stepper1.moveTo(400);
+  limswitch = LOW;
+  delay(2000);
+  while (digitalRead(limitPin) != HIGH && limit_count == 1) {
+    stepper1.rotate(-1);
+    Serial.print(limswitch);
+    Serial.println("Finding END2");
+  }
+  if (digitalRead(limitPin) == HIGH && limit_count == 1) {
+    stepper1.stop();
+    Serial.println("END POSITION2");
+    limit_count += 1;
+    limswitch = HIGH;
+    hOme = 2;
+  }
+  Serial.print("limit_count:"); Serial.println(limit_count);
+  Serial.print("limswitch:"); Serial.println(limswitch);
+  Serial.print("hOme:"); Serial.println(hOme);
+  Serial.print("POS:"); Serial.println(pos);
+  stepper1.setZero();
+  delay(2000);
+}
+
+void set_pos() {
+
+  while (hOme == 2 && limit_count > 1 && limswitch == HIGH && pos == 0) {
+
+    delay(1000);
+    stepper1.setSpeed(600);
+    stepper1.moveTo(3000);
+
+    while (stepper1.moving()) {
+      Serial.println("MOVING to Pos1");
+    }
+
+    pos = 1;
+    Serial.println("StartPositionSet, READY TO USE");
+    digitalWrite(solen,HIGH);
+  }
+  Serial.print("POS:"); Serial.println(pos);
+}
+
+void motor() {
+
+  if (!stepperRunning && !clothDetected)
+  {
+    // Set the flag to true when signature is not detected
+    clothDetected = true;
+    forwardStartTime = millis();
+    relayActivated = false;
+
+    // Move forward
+    stepper1.setSpeed(800);
+    stepper1.move(-targetPos);
+    Serial.println("Moving forward");
+    if (millis() - forwardStartTime >= 5500 && !relayActivated) {
+        digitalWrite(solen, LOW);
+        relayActivated = true;
+      }
+    cloth_count++;
+    stepperRunning = true;
+
+    while (stepper1.moving())
+    {
+      stepperPause.setTime(100); // Wait until the forward movement is complete
+    }
+    delay(500);
+    digitalWrite(solen,HIGH);
+
+    // Move backward
+    stepper1.setSpeed(1200);
+    stepper1.move(targetPos);
+    Serial.println("Moving backward");
+
+    while (stepper1.moving())
+    {
+      stepperPause.setTime(10); // Wait until the backward movement is complete
+    }
+
+    stepperRunning = false; // Reset the flag when the complete oscillation is done
+    relayActivated= false;
+  }
+}
+void collect() {
+  stepper1.setSpeed(1000);
+  stepper1.moveTo(-targetPos);
+  stepperPause.setTime(5000);
+  cloth_count = 0;
+  sTart = 0;
+  limit_count = 0;
+  hOme = 0;
+  pos = 0;
+}
